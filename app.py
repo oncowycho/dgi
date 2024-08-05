e11b6bb3d44a2966739b6ad2e38ef541ed76185d
oncowycho <143578451+oncowycho@users.noreply.github.com>
2024-08-05 18:43:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,18 @@ import plotly.graph_objects as go
 import base64
 from multiprocessing import Pool
 
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 def read_dose_dicom(dicom_file_path):
     ds = pydicom.dcmread(dicom_file_path)
