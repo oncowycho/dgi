823ef19fd2a84d67e86511439230e33a72eb203a
oncowycho <143578451+oncowycho@users.noreply.github.com>
2024-10-16 02:37:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,19 +13,6 @@ from pydicom.uid import ImplicitVRLittleEndian
 from dicompylercore import dicomparser, dvh
 import matplotlib.path
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 st.set_page_config(layout="wide")
 
 hide_github_icon = """
