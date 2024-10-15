8eb521c63e5b3ab055aeebe387372e67b5dec943
oncowycho <143578451+oncowycho@users.noreply.github.com>
2024-10-16 02:37:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,10 +15,13 @@ import matplotlib.path
 
 st.set_page_config(layout="wide")
 
-hide_github_icon = """
-.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+hide = """
+    <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+    </style>
 """
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+st.markdown(hide, unsafe_allow_html=True)
 
 def get_dvh(rtss, rtdose, roi, limit=None, callback=None):
     """Calculate a cumulative DVH in Gy from a DICOM RT Structure Set & Dose."""
